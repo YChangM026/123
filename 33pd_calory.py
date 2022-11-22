766601b510fa051bed6fa08040a057ff51de9c8e
YChangM026 <118792091+YChangM026@users.noreply.github.com>
2022-11-22 15:55:42 +0900

    Update and rename 33pd_calory.py to main.py

--- 33pd_calory.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-calo = {"첫째":100,"둘째":200,"셋째":300}
-
-#myVar = pd.Series(calo)
-myVar = pd.Series(calo, index=["첫째","둘째"])  #* 두 값을 지정함
-print(myVar)
-print(myVar[0])    #myVar[2] error
-print(myVar['둘째'])
